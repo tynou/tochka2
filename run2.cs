da2b9c0413ebcb73e43d7878738f1f6dbe371faf
Alexander Timonin <2sdaiwxet@gmail.com>
2025-10-31 19:08:44 +0500

    why

--- run2.cs
+++ run2.cs
@@ -4,158 +4,142 @@ using System.Linq;
 
 class Program
 {
-    private class Graph
-    {
-        public readonly Dictionary<char, HashSet<char>> AdjacencyList;
-        public readonly HashSet<char> Gates;
-
-        public Graph(List<(string, string)> edges)
-        {
-            AdjacencyList = new Dictionary<char, HashSet<char>>();
-            Gates = [];
-
-            var allNodes = new HashSet<char>();
-            foreach (var edge in edges)
-            {
-                allNodes.Add(edge.Item1[0]);
-                allNodes.Add(edge.Item2[0]);
-            }
-
-            foreach (var node in allNodes)
-            {
-                AdjacencyList[node] = [];
-                if (char.IsUpper(node))
-                    Gates.Add(node);
-            }
-
-            foreach (var edge in edges)
-            {
-                var u = edge.Item1[0];
-                var v = edge.Item2[0];
-                AdjacencyList[u].Add(v);
-                AdjacencyList[v].Add(u);
-            }
-        }
-
-        public void RemoveEdge(char u, char v)
-        {
-            if (AdjacencyList.TryGetValue(u, out var value1))
-                value1.Remove(v);
-
-            if (AdjacencyList.TryGetValue(v, out var value2))
-                value2.Remove(u);
-        }
-
-        public void MakeEdge(char u, char v)
-        {
-            AdjacencyList[u].Add(v);
-            AdjacencyList[v].Add(u);
-        }
-    }
-
-    private static string GetShortestPath(List<string> paths)
-    {
-        paths.Sort((p1, p2) =>
-        {
-            var gateCompare = p1.Last().CompareTo(p2.Last());
-            if (gateCompare != 0)
-                return gateCompare;
-
-            if (p1.Length > 1 && p2.Length > 1)
-                return p1[1].CompareTo(p2[1]);
-
-            return 0;
-        });
-
-        return paths[0];
-    }
-
-    private static List<string> GetPaths(char currentNode, Graph graph)
-    {
-        var paths = new List<string>();
-        var queue = new Queue<string>();
-        queue.Enqueue(currentNode.ToString());
-
-        while (queue.Count > 0)
-        {
-            var path = queue.Dequeue();
-            var lastNode = path.Last();
-
-            foreach (var neighbor in graph.AdjacencyList[lastNode].OrderBy(n => n))
-            {
-                if (path.Contains(neighbor))
-                    continue;
-
-                var newPath = path + neighbor;
-                if (graph.Gates.Contains(neighbor))
-                    paths.Add(newPath);
-                else
-                    queue.Enqueue(newPath);
-            }
-        }
-
-        return paths;
-    }
-
     static List<string> Solve(List<(string, string)> edges)
     {
-        var graph = new Graph(edges);
         var result = new List<string>();
-        var currentNode = 'a';
+        var adj = new Dictionary<string, HashSet<string>>();
+        foreach (var (node1, node2) in edges)
+        {
+            if (!adj.ContainsKey(node1)) adj[node1] = new HashSet<string>();
+            adj[node1].Add(node2);
+            if (!adj.ContainsKey(node2)) adj[node2] = new HashSet<string>();
+            adj[node2].Add(node1);
+        }
+        var gates = adj.Keys.Where(node => char.IsUpper(node[0])).ToHashSet();
+        var virusPosition = "a";
 
         while (true)
         {
-            if (GetPaths(currentNode, graph).Count == 0)
+            if (FindPathToClosestGate(adj, virusPosition, gates).Length == 0)
                 break;
+            
+            var possibleMoves = new List<string>();
+            foreach (var gate in gates.OrderBy(g => g))
+                if (adj.TryGetValue(gate, out var neighbors))
+                    foreach (var neighbor in neighbors.OrderBy(n => n))
+                        possibleMoves.Add($"{gate}-{neighbor}");
 
-            var cutOptions = new List<(char, char)>();
-            foreach (var gate in graph.Gates.OrderBy(g => g))
-                cutOptions.AddRange(graph.AdjacencyList[gate].OrderBy(n => n).Select(adjacent => (gate, adjacent)));
-
-            foreach (var (gate, adj) in cutOptions)
+            string bestMove = null;
+            foreach (var move in possibleMoves)
             {
-                graph.RemoveEdge(gate, adj);
-
-                var paths = GetPaths(currentNode, graph);
-                if (paths.Count == 0)
-                {
-                    result.Add($"{gate}-{adj}");
-                    break;
-                }
-
-                var chosenPath = GetShortestPath(paths);
-                if (chosenPath.Length > 2)
-                {
-                    result.Add($"{gate}-{adj}");
-                    currentNode = chosenPath[1];
-                    break;
-                }
-
-                graph.MakeEdge(gate, adj);
+                var adjCopy = new Dictionary<string, HashSet<string>>();
+                foreach (var item in adj)
+                    adjCopy[item.Key] = new HashSet<string>(item.Value);
+                var parts = move.Split('-');
+                var node1 = parts[0];
+                var node2 = parts[1];
+                adjCopy[node1].Remove(node2);
+                adjCopy[node2].Remove(node1);
+                var virusMovePath = FindPathToClosestGate(adjCopy, virusPosition, gates);
+                if (virusMovePath.Length == 1)
+                    continue;
+                bestMove = move;
+                break;
             }
+            var bestMoveParts = bestMove.Split('-');
+            var gateToCut = bestMoveParts[0];
+            var nodeToCut = bestMoveParts[1];
+            adj[gateToCut].Remove(nodeToCut);
+            adj[nodeToCut].Remove(gateToCut);
+            result.Add(bestMove);
+            var nextVirusPath = FindPathToClosestGate(adj, virusPosition, gates);
+            if (nextVirusPath.Length > 0)
+                virusPosition = nextVirusPath[0].Item2;
         }
 
         return result;
     }
 
+    private static (string, string)[] FindPathToClosestGate(Dictionary<string, HashSet<string>> edgesDict,
+        string position,
+        ISet<string> gates)
+    {
+        var pathsToGates = new List<(string, string)[]>();
+        foreach (var gate in gates)
+        {
+            var path = FindMinPathToGate(edgesDict, position, gate);
+            if (path.Length > 0)
+                pathsToGates.Add(path);
+        }
+
+        if (pathsToGates.Count == 0)
+            return [];
+        pathsToGates
+            .Sort((x, y) =>
+            {
+                if (x.Length.CompareTo(y.Length) != 0) return x.Length.CompareTo(y.Length);
+                return x.Last().Item2[0].CompareTo(y.Last().Item2[0]);
+            });
+        return pathsToGates[0];
+    }
+
+    private static (string, string)[] FindMinPathToGate(Dictionary<string, HashSet<string>> edgesDict,
+        string position, string gate)
+    {
+        if (position == gate)
+            return new List<(string, string)>().ToArray();
+        var queue = new Queue<string>();
+        var visited = new Dictionary<string, string>();
+        var pathEdges = new List<(string, string)>();
+        queue.Enqueue(position);
+        visited[position] = null;
+        while (queue.Count > 0)
+        {
+            var current = queue.Dequeue();
+            if (current == gate)
+            {
+                var node = gate;
+                while (visited[node] != null)
+                {
+                    pathEdges.Add((visited[node], node));
+                    node = visited[node];
+                }
+
+                pathEdges.Reverse();
+                return pathEdges.ToArray();
+            }
+
+            if (edgesDict.TryGetValue(current, out var value))
+                foreach (var neighbor in value.OrderBy(n => n))
+                    if (visited.TryAdd(neighbor, current))
+                        queue.Enqueue(neighbor);
+        }
+
+        return [];
+    }
+
     static void Main()
     {
         var edges = new List<(string, string)>();
-        string? line;
+        string line;
 
-        while ((line = Console.ReadLine()) != null && line != "")
+        while ((line = Console.ReadLine()) != null && line.Length > 0)
         {
             line = line.Trim();
             if (!string.IsNullOrEmpty(line))
             {
                 var parts = line.Split('-');
                 if (parts.Length == 2)
+                {
                     edges.Add((parts[0], parts[1]));
+                }
             }
         }
 
         var result = Solve(edges);
         foreach (var edge in result)
+        {
             Console.WriteLine(edge);
+        }
     }
 }
